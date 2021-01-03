83c471897b4c8f4a84ff75b20225d70bc6bf734d
Ninngi <thepatrvq@gmail.com>
2021-01-03 14:25:36 +0100

    ServicesSection

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import AboutSection from '../components/AboutSection.js'
+import ServicesSection from '../components/ServicesSection.js'
 
 const AboutUs = () => {
   return (
     <>
       <AboutSection />
+      <ServicesSection />
     </>
   )
 }
