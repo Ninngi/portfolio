33fa19e82d3a81302f25a4dd0c9505cbade70317
Ninngi <thepatrvq@gmail.com>
2021-01-03 14:35:07 +0100

    FaqSection

---
+++ src/components/FaqSection.js
@@ -0,0 +1,61 @@
+import React from 'react'
+
+const FaqSection = () => {
+  return (
+    <div className='faq'>
+      <h2>
+        Any Questions <span>FAQ</span>
+      </h2>
+      <div className='question'>
+        <h4>How Do I Start? </h4>
+        <div className='answer'>
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Sint deserunt minim culpa Lorem commodo non sit ipsum adipisicing
+            adipisicing incididunt. Reprehenderit ad nostrud exercitation dolor
+            consequat deserunt. Aute minim laborum amet ea duis laborum
+            incididunt nisi cillum excepteur sint nostrud voluptate.
+          </p>
+        </div>
+      </div>
+      <div className='question'>
+        <h4>Payment methods</h4>
+        <div className='answer'>
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Sint deserunt minim culpa Lorem commodo non sit ipsum adipisicing
+            adipisicing incididunt. Reprehenderit ad nostrud exercitation dolor
+            consequat deserunt. Aute minim laborum amet ea duis laborum
+            incididunt nisi cillum excepteur sint nostrud voluptate.
+          </p>
+        </div>
+      </div>
+      <div className='question'>
+        <h4>How Deep? </h4>
+        <div className='answer'>
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Sint deserunt minim culpa Lorem commodo non sit ipsum adipisicing
+            adipisicing incididunt. Reprehenderit ad nostrud exercitation dolor
+            consequat deserunt. Aute minim laborum amet ea duis laborum
+            incididunt nisi cillum excepteur sint nostrud voluptate.
+          </p>
+        </div>
+      </div>
+      <div className='question'>
+        <h4>How Do I Start?XXX </h4>
+        <div className='answer'>
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Sint deserunt minim culpa Lorem commodo non sit ipsum adipisicing
+            adipisicing incididunt. Reprehenderit ad nostrud exercitation dolor
+            consequat deserunt. Aute minim laborum amet ea duis laborum
+            incididunt nisi cillum excepteur sint nostrud voluptate.
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default FaqSection
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import AboutSection from '../components/AboutSection.js'
 import ServicesSection from '../components/ServicesSection.js'
+import FaqSection from '../components/FaqSection.js'
 
 const AboutUs = () => {
   return (
     <>
       <AboutSection />
       <ServicesSection />
+      <FaqSection />
     </>
   )
 }
